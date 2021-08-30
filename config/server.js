612e418b38c324f7fcc2ba0e1d5f65e40fbe7146
winterandrea <andrecasero@gmail.com>
2021-08-30 14:59:30 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '800bac97c312a7d4462c318519125993'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1c2b8d30-826a-4609-b04f-a71f62f911a9'
+};
